02167d0bd9f55827d6ada745c6dc90c978013419
Andi Plummer <andrea@andreaplummer.com>
2019-11-20 18:06:12 -0500

    doneeee

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-const db = new Sequelize('postgres://localhost:5432/skinRx-server', { logging: false })
+const db = new Sequelize('postgres://localhost:5432/skinrx-server', { logging: false })
  
 module.exports = {
    db
