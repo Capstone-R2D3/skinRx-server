701426aee2f16f7901bfa0fac6d314c852e5b2c3
Andi Plummer <andrea@andreaplummer.com>
2019-11-20 17:57:22 -0500

    this better work

--- api/products.js
+++ api/products.js
@@ -3,8 +3,9 @@ const router = require('express').Router()
 
 router.get('/', async (req, res, next) => {
   try {
-    const products = await Products.findAll()
-    res.json(products)
+    console.log('here')
+    // const products = await Products.findAll()
+    // res.json(products)
   } catch (error) {
     next(error)
   }
