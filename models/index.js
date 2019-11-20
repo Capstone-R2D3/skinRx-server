b235b00526c84b82d6f6f53c0ec32bc3cf92063c
Andi Plummer <andrea@andreaplummer.com>
2019-11-20 18:09:12 -0500

    still trying

--- models/index.js
+++ models/index.js
@@ -1,5 +1,16 @@
 const Sequelize = require('sequelize')
-const db = new Sequelize('postgres://localhost:5432/skinrx-server', { logging: false })
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  db = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    logging:  true //false
+  });
+} else {
+  // the application is executed on the local machine
+  db = new Sequelize('postgres://localhost:5432/skinrx-server');
+}
+// const db = new Sequelize('postgres://localhost:5432/skinrx-server', { logging: false })
  
 module.exports = {
    db
