2013a502fbd9d6baecc798b1e85ef6b581d867e7
Andi Plummer <andrea@andreaplummer.com>
2019-11-22 12:21:07 -0500

    sending user

--- api/users.js
+++ api/users.js
@@ -31,7 +31,7 @@ router.post('/login', async (req, res, next) => {
 router.post('/signup', async (req, res, next) => {
   try {
     const user = await Users.create(req.body);
-    res.sendStatus(201)
+    res.json(user)
   } catch (err) {
     if (err.name === 'SequelizeUniqueConstraintError') {
       res.status(401).send('User already exists');
