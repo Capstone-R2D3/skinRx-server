8a7d0698bb8e8691f60795c3a13b135d66bd5405
Andi Plummer <andrea@andreaplummer.com>
2019-11-25 16:15:29 -0500

    added route to get product by id

--- api/products.js
+++ api/products.js
@@ -11,6 +11,17 @@ router.get('/', async (req, res, next) => {
   }
 })
 
+router.get('/:id', async (req, res, next) => {
+  try {
+    const product = await Products.findByPk({
+      where: { id: req.params.id }
+    })
+    res.json(product)
+  } catch (error) {
+    next(error)
+  }
+})
+
 router.post('/', async (req, res, next) => {
   try {
     const newProduct = await Products.create(req.body);
