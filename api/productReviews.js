16ac46581513b1739f9f42c3e64b8aac842abac0
Andi Plummer <andrea@andreaplummer.com>
2019-11-25 16:54:33 -0500

    added get all ratings and get rating by product id routes

--- api/productReviews.js
+++ api/productReviews.js
@@ -1,6 +1,25 @@
 const router = require('express').Router();
-const ProductReviews = require('../models/productReviews');
+const ProductReviews = require('../models/associations');
 
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await ProductReviews.findAll()
+    res.json(products)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const rating = await ProductReviews.findAll({
+      where: { productId: req.params.id }
+    })
+    res.json(rating)
+  } catch (error) {
+    next(error)
+  }
+})
 
 router.post('/', async (req, res, next) => {
     try {
