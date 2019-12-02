614664c6cd2d75553de2d37607008e3874f7ff17
sjuhye <sjuhye@gmail.com>
2019-12-02 13:01:29 -0500

    updated ingredients API

--- api/ingredients.js
+++ api/ingredients.js
@@ -1,10 +1,30 @@
 const {Ingredients} = require('../models/associations');
 const router = require('express').Router();
 
-router.get('/', async (req, res, next) => {
+// router.get('/', async (req, res, next) => {
+//     try {
+//         const ingredients = await Ingredients.findAll();
+//         res.json(ingredients);
+//     } catch (error) {
+//         next(error);
+//     }
+// })
+
+router.get('/', (req, res, next) => {
     try {
-        const ingredients = await Ingredients.findAll();
-        res.json(ingredients);
+        const { ingredientsToFind } = req.body;
+        const ingredientsData = ingredientsToFind.reduce(async (accum, ingredient) => {
+            const data = await Ingredients.findOne({
+                where: {
+                    name: ingredient
+                }
+            })
+            if (data) {
+                accum.push(data);
+            }
+            return accum;
+        }, []);
+        res.json(ingredientsData)
     } catch (error) {
         next(error);
     }
