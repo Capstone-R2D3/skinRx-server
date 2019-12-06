683d5c7f70f31fac434d18b5a8c7d098081028ce
Andi Plummer <andrea@andreaplummer.com>
2019-12-06 12:17:25 -0500

    edited date format in get single entry route

--- api/entries.js
+++ api/entries.js
@@ -3,9 +3,9 @@ const JourneyEntries = require('../models/journey-entries')
 
 router.get('/:userId/entries/:date', async (req, res, next) => {
     try {
-      // const dateStr = req.params.date.split("-")
-      // let newDate = `${dateStr[1]}/${dateStr[1]}/${dateStr[0]}`
-      const oneEntry = await JourneyEntries.findAll({ where: { userId: req.params.userId, date: req.params.date }})
+      const dateStr = req.params.date.split("-")
+      let newDate = `${dateStr[0]}/${dateStr[1]}/${dateStr[2]}`
+      const oneEntry = await JourneyEntries.findAll({ where: { userId: req.params.userId, date: newDate }})
       res.json(oneEntry)
     } catch (error) {
       next (error)
